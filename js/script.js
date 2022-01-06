8a1437a8eda6a0ec0bdcae853db6523bdc3f380a
Ulrack1404 <88722734+Ulrack1404@users.noreply.github.com>
2022-01-06 16:24:46 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+   $('.icon-menu').click(function (event) {
+      $('.menu__body, .icon-menu').toggleClass('_active');  
+      $('body').toggleClass('lock');      
+   }); 
+});  
